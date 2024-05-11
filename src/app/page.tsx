65b9f4d39f40deb0a2ab76eff57c565a68276ce0
Walace Cordeiro dos Santos <walacecordeirodossantos@gmail.com>
2024-05-11 11:38:28 -0300

    função de deletar cliente adicionada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState, useRef, FormEvent } from "react";
 import { api } from "@/services/api";
 import { FiTrash } from "react-icons/fi";
+import Alert from "sweetalert2";
 
 interface CustomersProps {
  id: string;
@@ -40,6 +41,37 @@ export default function Home() {
   });
 
   setCustomers((allCustomers) => [...allCustomers, response.data]);
+  nameRef.current.value = "";
+  emailRef.current.value = "";
+ }
+
+ async function handleDelete(id: string) {
+  Alert.fire({
+   title: "Tem certeza que quer deletar?",
+   text: "Você não poderá reverter isso!",
+   icon: "warning",
+   iconColor: "#EF4444",
+   showCancelButton: true,
+   confirmButtonColor: "#86EFAC",
+   cancelButtonColor: "#EF4444",
+   confirmButtonText: "Sim, deletar",
+   cancelButtonText: "Não, cancelar",
+  }).then(async (result) => {
+   if (result.isConfirmed) {
+    try {
+     await api.delete("/customer", {
+      params: {
+       id: id,
+      },
+     });
+
+     const allCustomers = customers.filter((customer) => customer.id !== id);
+     setCustomers(allCustomers);
+    } catch (err) {
+     console.log(err);
+    }
+   }
+  });
  }
 
  return (
@@ -52,31 +84,37 @@ export default function Home() {
       Nome:{" "}
      </label>
      <input
-      className="w-full mb-5 p-2 rounded"
       id="name"
       type="text"
       placeholder="Digite seu nome completo"
       ref={nameRef}
+      autoComplete="name"
+      className="w-full mb-5 p-2 rounded"
      />
-     <label htmlFor="name" className="font-medium text-white">
+     <label htmlFor="email" className="font-medium text-white">
       Email:{" "}
      </label>
      <input
-      id="name"
+      id="email"
       type="email"
       placeholder="Digite seu email principal"
-      className="w-full mb-5 p-2 rounded"
       ref={emailRef}
+      autoComplete="email"
+      className="w-full mb-5 p-2 rounded"
      />
 
-     <input type="submit" className="cursor-pointer w-full p-2 bg-green-500 rounded font-medium" />
+     <input
+      value="Cadastrar"
+      type="submit"
+      className="cursor-pointer w-full p-2 bg-green-300 rounded font-medium hover:scale-[1.03] duration-200"
+     />
     </form>
 
     <section className="flex flex-col-reverse gap-4">
      {customers.map((customer) => (
       <article
        key={customer.id}
-       className="w-full bg-white rounded p-2 relative hover:scale-105 duration-200"
+       className="w-full bg-white rounded p-2 relative hover:scale-[1.03] duration-200"
       >
        <p>
         <span className="font-medium">Nome: </span> {customer.name}
@@ -88,7 +126,10 @@ export default function Home() {
         <span className="font-medium">Status: </span> {customer.status ? "Ativo" : "Inativo"}
        </p>
 
-       <button className="bg-red-500 w-7 h-7 flex items-center justify-center rounded-lg absolute -top-2 right-2">
+       <button
+        onClick={() => handleDelete(customer.id)}
+        className="bg-red-500 w-7 h-7 flex items-center justify-center rounded-lg absolute -top-2 right-2"
+       >
         <FiTrash size={18} color="fff" />
        </button>
       </article>
